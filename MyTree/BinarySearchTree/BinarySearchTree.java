0f00f86bd7dcb995cc23670ccb8046860d68dc45
sachinsharma01 <sachin2252sharma@gmail.com>
2021-10-26 12:58:33 +0530

    added the minimum depth function to the BinarySearchTree

--- MyTree/BinarySearchTree/BinarySearchTree.java
+++ MyTree/BinarySearchTree/BinarySearchTree.java
@@ -81,6 +81,18 @@ public class BinarySearchTree {
         }
     }
 
+    public int minDepth(Node root) {
+        if (root == null)
+            return 0;
+        if (root.left == null && root.right == null)
+            return 1;
+        if (root.left == null)
+            return minDepth(root.right) + 1;
+        if (root.right == null)
+            return minDepth(root.left) + 1;
+        return 0;
+    }
+
     public static void main(String args[])
     {
         BinarySearchTree tree = new BinarySearchTree();
